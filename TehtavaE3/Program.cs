56169168d5fcb5851c36bcdb4270e539edb926f7
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-25 08:40:40 +0300

    Add project files.

---
+++ TehtavaE3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EtuJaSukunimiKirjain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Syötä sana: ");
+            string sana = Console.ReadLine();
+
+            if (sana.Length < 2)
+            {
+                Console.WriteLine("Sana on liian lyhyt vaihdettavaksi.");
+            }
+            else
+            {
+                char ensimmäinenKirjain = sana[0];
+                char viimeinenKirjain = sana[sana.Length - 1];
+
+                // Luo uusi sana vaihtamalla ensimmäinen ja viimeinen kirjain
+                string uusiSana = viimeinenKirjain + sana.Substring(1, sana.Length - 2) + ensimmäinenKirjain;
+
+                Console.WriteLine("Uusi sana: " + uusiSana);
+            }
+        }
+    }
+}
+
